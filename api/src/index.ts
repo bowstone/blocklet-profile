63ffd4fff7d27e44df2c235e664f43cf1cc9e950
shipeng <648385129@qq.com>
2025-01-06 12:09:19 +0800

    feat: 限流

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,11 @@
 import fallback from '@blocklet/sdk/lib/middlewares/fallback';
+import { BizStatusCode, IResponseData } from '@types';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv-flow';
 import express, { ErrorRequestHandler } from 'express';
 import 'express-async-errors';
+import { rateLimit } from 'express-rate-limit';
 import path from 'path';
 
 import { logger } from './libs';
@@ -22,6 +24,16 @@ app.use(express.json({ limit: '1 mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1 mb' }));
 app.use(cors());
 
+// 限流
+app.use(
+  rateLimit({
+    windowMs: 3 * 60 * 1000,
+    limit: 1000,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+    // store: ... , // Redis, Memcached, etc. See below.
+  }),
+);
 const router = express.Router();
 router.use('/api', routes);
 app.use(router);
@@ -32,14 +44,17 @@ if (isProduction) {
   const staticDir = path.resolve(process.env.BLOCKLET_APP_DIR!, 'dist');
   app.use(express.static(staticDir, { maxAge: '30d', index: false }));
   app.use(fallback('index.html', { root: staticDir }));
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  app.use(((err, _req, res, _next) => {
-    logger.error(err.stack);
-    res.status(500).send('Something broke!');
-  }) as ErrorRequestHandler);
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use(((err, _req, res, _next) => {
+  logger.error(err.stack);
+  res.status(500).send({
+    code: BizStatusCode.ServiceError,
+    message: '服务异常，请稍后重试',
+    data: null,
+  } as IResponseData);
+}) as ErrorRequestHandler);
 const port = parseInt(process.env.BLOCKLET_PORT!, 10);
 
 export const server = app.listen(port, (err?: any) => {
